560e475fdff9d6f53c8996583a72678122eddd11
Robin Spinks <uglybluecat@googlemail.com>
2017-05-03 21:28:19 +0100

    Network Manager

--- TDD GD Test/NetworkManager.swift
+++ TDD GD Test/NetworkManager.swift
@@ -9,5 +9,119 @@
 import Foundation
 
 class NetworkManager {
-    init() {}
+    
+    enum Method: String {
+        case GET, POST, PUT, DELETE
+    }
+    
+    let urlSession : URLSession
+    
+    init() {
+        self.urlSession = URLSession(configuration: .default)
+    }
+    
+    /**
+     Calls an HTTP request with the POST method
+     
+     - parameters:
+         - headers: The http request headers
+         - params: The parameters for the POST request
+         - body: The body for the http request
+         - urlString: the URL of the POST request
+         - completion: A method to handle the returned data
+     */
+    func postRequest (headers: Dictionary<String, String>,
+                      params: Dictionary<String, String>,
+                      body : Data,
+                      urlStr: String,
+                      completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        makeRequest(method: .POST,
+                    headers: headers,
+                    params: params,
+                    body : body,
+                    urlStr: urlStr,
+                    completion: completion)
+    }
+    
+    /**
+     Calls an HTTP request with the GET method
+     
+     - parameters:
+         - headers: The http request headers
+         - params: The parameters for the GET request
+         - body: The body for the http request
+         - urlString: the URL of the GET request
+         - completion: A method to handle the returned data
+     */
+    func getRequest (headers: Dictionary<String, String>,
+                     params: Dictionary<String, String>,
+                     body : Data,
+                     urlStr: String,
+                     completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        makeRequest(method: .GET,
+                    headers: headers,
+                    params: params,
+                    body : body,
+                    urlStr: urlStr,
+                    completion: completion)
+    }
+    
+    /**
+     Makes an HTTP request with the provided parameters
+     
+     - parameters:
+         - method: The method for the request
+         - headers: The http request headers
+         - params: The parameters for the request
+         - body: The body for the http request
+         - urlStr: the URL of the request
+         - completion: A method to handle the returned data
+     */
+    func makeRequest(method: Method,
+                     headers: Dictionary<String, String>,
+                     params: Dictionary<String, String>,
+                     body : Data,
+                     urlStr: String,
+                     completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        
+        if var urlComponents = URLComponents(string: urlStr) {
+            
+            var requestParams: [URLQueryItem] = []
+            
+            for (paramName, paramValue) in params {
+                requestParams.append(URLQueryItem(name: paramName, value: paramValue))
+            }
+            
+            urlComponents.queryItems = requestParams
+            
+            if let url : URL = urlComponents.url {
+                var request : URLRequest = URLRequest(url: url)
+                request.httpMethod = method.rawValue
+                request.httpBody = body
+                
+                for (key, value) in headers {
+                    request.setValue(value, forHTTPHeaderField: key)
+                }
+                
+                self.handleRequest(request: request, completion: completion)
+            } else {
+                print("Could not obtain NSURL from \(urlComponents.debugDescription)")
+            }
+        } else {
+            print("Could not construct NSURLComponents from \(urlStr)")
+        }
+    }
+    
+    /**
+     Handles an HTTP request of any type
+     
+     - parameters:
+         - request: The HTTP request
+         - completion: A method to handle the returned data
+     */
+    func handleRequest (request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        
+        let task : URLSessionDataTask = urlSession.dataTask(with: request, completionHandler: completion)
+        task.resume()
+    }
 }
--- TDD GD TestTests/Networking/NetworkManagerTests.swift
+++ TDD GD TestTests/Networking/NetworkManagerTests.swift
@@ -17,4 +17,69 @@ class NetworkManagerTests: XCTestCase {
         XCTAssertNotNil(networkManager)
     }
     
+    func testURLSession() {
+        XCTAssertNotNil(networkManager.urlSession)
+    }
+    
+    // Test relies on network connectivity - possibly improved by mocking
+    func testCanHandleRequest() {
+        if let url : URL = URL(string: "https://account-live.cpcconnect.net/auth/token") {
+            let expect = expectation(description: "waitForNetworkManager")
+            
+            var testData : Data = Data()
+            let urlRequest : URLRequest = URLRequest(url: url)
+            networkManager.handleRequest(request: urlRequest, completion: {
+                (data, urlResponse, error) in
+                guard error == nil else {
+                    XCTFail("error: \(String(describing: error))")
+                    return
+                }
+                guard data != nil else {
+                    XCTFail("null data")
+                    return
+                }
+                testData = data!
+                expect.fulfill()
+            })
+            
+            waitForExpectations(timeout: 15, handler: nil)
+            XCTAssert(testData.count > 0)
+        } else {
+            XCTFail("")
+        }
+    }
+    
+    // Test relies on network connectivity - possibly improved by mocking
+    func testRequestHandling() {
+        let expect = expectation(description: "waitForNetworkManager")
+        
+        var testData : Data = Data()
+        let apiURL : String = "https://account-live.cpcconnect.net/auth/token"
+        let params : Dictionary<String, String> = [:]
+        let headers : Dictionary<String, String> = ["Content-Type" : "application/x-www-form-urlencoded"]
+        let jsonBody = ["grant_type" : "password",
+                        "username" : "oojamaflip2008",
+                        "password" : "TestPassword123!"]
+        let body : Data = NSKeyedArchiver.archivedData(withRootObject: jsonBody)
+        
+        networkManager.postRequest(headers: headers,
+                                   params: params,
+                                   body: body,
+                                   urlStr: apiURL) {
+            (data, urlResponse, error) in
+            guard error == nil else {
+                XCTFail("error: \(String(describing: error))")
+                return
+            }
+            guard data != nil else {
+                XCTFail("null data")
+                return
+            }
+            testData = data!
+            expect.fulfill()
+        }
+        
+        waitForExpectations(timeout: 15, handler: nil)
+        XCTAssert(testData.count > 0)
+    }
 }
